acf6094a51c17871b4bcaf07c824d31649f100e8
Emad Kadyear <eksaber87@gmail.com>
2025-01-18 19:12:17 +0100

    ADD Base-FIre

---
+++ firebaseConfig.js
@@ -0,0 +1,23 @@
+/** @format */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCImUrnDBt_VRYUP8b0ssvTeYJPsB0lVXs",
+  authDomain: "api-pokemon-c4419.firebaseapp.com",
+  projectId: "api-pokemon-c4419",
+  storageBucket: "api-pokemon-c4419.firebasestorage.app",
+  messagingSenderId: "480601322972",
+  appId: "1:480601322972:web:b85c9725730c6053c18b19",
+  measurementId: "G-XRZLBFPSX1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
